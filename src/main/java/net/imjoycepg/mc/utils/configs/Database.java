1eb0f241b208317008df88a0dca2e0bc563cf407
ImJoycePG <104446699+ImJoycePG@users.noreply.github.com>
2022-09-27 14:53:17 +0200

    Fix port MySQL

--- src/main/java/net/imjoycepg/mc/utils/configs/Database.java
+++ src/main/java/net/imjoycepg/mc/utils/configs/Database.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @ToString
 public class Database {
     private String host = "127.0.0.1";
-    private String port = "3006";
+    private String port = "3306";
     private String username = "root";
     private String password = "THIS_PASSWORD";
     private String database = "DorisRooms";
